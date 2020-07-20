9a91d26ae36344feb3a980cd906d9af12a29fbe0
Giordano <giordanoamaral@gmail.com>
2020-07-20 15:14:02 -0300

    Aulas 1,2 threads

---
+++ src/Thread2/src/App.java
@@ -0,0 +1,42 @@
+package Thread2.src;
+
+import java.util.Scanner;
+
+class Processor extends Thread {
+
+  private boolean running = true;
+
+  public void run() {
+    while (running) {
+      System.out.println("Hello! ");
+      try {
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+
+  }
+
+  public void shotdown() {
+    running = false;
+  }
+
+}
+
+
+public class App {
+
+  public static void main(String[] args) {
+
+    Processor proc1 = new Processor();
+    proc1.start();
+
+    System.out.println("Press return to stop ...");
+    Scanner scanner = new Scanner(System.in);
+    scanner.nextLine();
+
+    proc1.shotdown();
+  }
+
+}
